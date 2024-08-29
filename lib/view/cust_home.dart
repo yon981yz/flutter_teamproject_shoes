f585be17cc1219f1df28ceba74505183bf449a07
LrZl <dingtti@naver.com>
2024-08-29 15:52:05 +0900

    update
    o

---
+++ lib/view/cust_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustHome extends StatefulWidget {
+  const CustHome({super.key});
+
+  @override
+  State<CustHome> createState() => _CustHomeState();
+}
+
+class _CustHomeState extends State<CustHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
