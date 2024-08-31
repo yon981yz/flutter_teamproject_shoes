ae2ec136a96721789b2f121c2caf368dc102d6b5
tjdduq5433 <tjdduq5433@naver.com>
2024-08-31 13:34:40 +0900

    page fixing
    fixing a little bit

--- lib/view/user_product_details.dart
+++ lib/view/user_product_details.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+<<<<<<< HEAD
 import 'package:flutter_teamproject_shoes/view/user_choice_branch.dart';
 import 'package:flutter_teamproject_shoes/vm/customer_handler.dart';
 import 'package:get/get.dart';
 
+=======
+import 'package:flutter_teamproject_shoes/vm/database_handler.dart';
+>>>>>>> 1b603f7 (page fixing)
 
-class UserProductDetails extends StatefulWidget {
-  const UserProductDetails({super.key});
+class Home extends StatefulWidget {
+  const Home({super.key});
 
   @override
-  State<UserProductDetails> createState() => _UserProductDetailsState();
+  State<Home> createState() => _HomeState();
 }
 
+<<<<<<< HEAD
 class _UserProductDetailsState extends State<UserProductDetails> {
   //Property
   late CustomerHandler handler;
@@ -23,10 +28,24 @@ class _UserProductDetailsState extends State<UserProductDetails> {
     handler=CustomerHandler();
   }
 
+=======
+class _HomeState extends State<Home> {
+    late DatabaseHandler handler;
+
+    @override
+  void initState() {
+    super.initState();
+    handler = DatabaseHandler();
+  }
+
+
+
+>>>>>>> 1b603f7 (page fixing)
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+<<<<<<< HEAD
         title: const Text(
             'SB Market',
             style: TextStyle(
@@ -81,6 +100,56 @@ class _UserProductDetailsState extends State<UserProductDetails> {
               )
               )
           ],
+=======
+        title: const Text('ss'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  /* Image.asset(
+                    width: 150,
+                    height: 100,
+                  ), */
+                  Text('코르테즈'),
+                ],
+              ),
+              /* Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Image.asset(
+                  'images/pika.png',
+                  width: 150,
+                  height: 150,
+                ),
+              ), */
+              const Text('가격'),
+              const Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Text('품번'),
+              ),
+              const Text('컬러'),
+              const Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Text('사이즈'),
+              ),
+              ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                  ),
+                  child: const Text(
+                    '구매하기',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ))
+            ],
+          ),
+>>>>>>> 1b603f7 (page fixing)
         ),
       ),
     );
